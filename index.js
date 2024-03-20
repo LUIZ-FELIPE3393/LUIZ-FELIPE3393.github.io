97297e887e93fae953a36596f73b4e7c184b8703
LUIZ-FELIPE3393 <2023003393@ifam.edu.br>
2024-03-20 15:34:22 -0400

    Leitura de dados do checklist no backend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const cookieParser = require('cookie')
 
 const saveChecklist = require('./checklist/save_checklist');
 
@@ -11,9 +12,13 @@ server.listen(3030, function() {
     console.log('O servidor já pode ser acessado em http://localhost:3030');
 });
 
+//Session
+
+
 //Bodyparser
     app.use(express.urlencoded({extended: false}))
     app.use(express.json());
+    app.use(cookieParser());
 
 //Bootstrap
     app.use('/css', express.static('./bootstrap/css'));
@@ -26,7 +31,17 @@ server.listen(3030, function() {
 //Rotas
 app.post('/save-checklist', function(req, res) {
     res.send(req.body.datadia);
-    console.log(req);
+    console.log(req.body.datadia);
+    console.log(req.body.caixa);
+    console.log(req.body.galao);
+    console.log(req.body.vaso);
+    console.log(req.body.balde);
+});
+
+app.get('/cookie',function(req, res){
+    let minute = 60 * 1000;
+    res.cookie(cookie_name, 'cookie_value', { secure: true });
+    return res.send('cookie has been set!');
 });
 
 app.get('/checklist', function(req,res,html) {
--- public/js/checklist.js
+++ public/js/checklist.js
@@ -1,75 +1,107 @@
 const addButton = document.querySelector('#add-checklist');
 const checklistTable = document.querySelector('#checklist-table');
+const form = document.querySelector("#formChecklist");
+const saveButtonWeb = document.querySelector('#save-checklist-web');
 
 let tupla_num = 1;
+
+form.addEventListener('submit', function(e) {
+    // Transforma valores de check nulo em 0
+    for (let tuple of document.querySelectorAll("tr[name=tupla]")) {
+        for (let checkbox of tuple.querySelectorAll("input[type=checkbox]")) {
+            if (!checkbox.checked) {
+                checkbox.value = 0;
+            } else {
+                for (let hiddenNode of tuple.querySelectorAll("input[type=hidden]")) {
+                    if (hiddenNode.id == checkbox.id) {
+                        console.log(hiddenNode);
+                        hiddenNode.disabled = true;
+                    }
+                }
+            }
+        }
+    }
+
+});
 
 addButton.addEventListener('click', function(e) {
     let tuple = document.createElement("tr");
     tuple.setAttribute("id", "tupla-" + tupla_num);
+    tuple.setAttribute("name", "tupla");
     tupla_num++;
     tuple.innerHTML = tupleFormat; 
     checklistTable.append(tuple);
-    console.log(e);
 });
 
 const tupleFormat = `
 <td class="td-data">
     <div class="form-check form-check-inline">
-        <input class="form-text-input" type="text" name="datadia" id="datadia">
+        <input class="form-text-input" type="text" name="datadia" id="datadia" required>
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" name="caixa" id="caixa" value="v-caixa">
+        <input type="hidden" name="caixa" id="caixa" value="0">
+        <input class="form-check-input" type="checkbox" name="caixa" id="caixa" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" name="galao" id="galao" value="v-galao">
+        <input type="hidden" name="galao" id="galao" value="0">
+        <input class="form-check-input" type="checkbox" name="galao" id="galao" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline"> 
-        <input class="form-check-input" type="checkbox" name="vaso" id="vaso" value="v-vaso">
+        <input type="hidden" name="vaso" id="vaso" value="0">
+        <input class="form-check-input" type="checkbox" name="vaso" id="vaso" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" name="balde" id="balde" value="v-balde">
+        <input type="hidden" name="balde" id="balde" value="0">
+        <input class="form-check-input" type="checkbox" name="balde" id="balde" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" name="garrafa" id="garrafa" value="v-garrafa">
+        <input type="hidden" name="garrafa" id="garrafa" value="0">
+        <input class="form-check-input" type="checkbox" name="garrafa" id="garrafa" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" name="pneu" id="pneu" value="v-pneu">
+        <input type="hidden" name="pneu" id="pneu" value="0">
+        <input class="form-check-input" type="checkbox" name="pneu" id="pneu" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" name="piscina" id="piscina" value="v-piscina">
+        <input type="hidden" name="piscina" id="piscina" value="0">
+        <input class="form-check-input" type="checkbox" name="piscina" id="piscina" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" name="pocas" id="pocas" value="v-pocas">
+        <input type="hidden" name="pocas" id="pocas" value="0">
+        <input class="form-check-input" type="checkbox" name="pocas" id="pocas" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" name="pote" id="pote" value="v-pote">
+        <input type="hidden" name="pote" id="pote" value="0">
+        <input class="form-check-input" type="checkbox" name="pote" id="pote" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" name="entulho" id="entulho" value="v-entulho">
+        <input type="hidden" name="entulho" id="entulho" value="0">
+        <input class="form-check-input" type="checkbox" name="entulho" id="entulho" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" name="calha" id="calha" value="v-calha">
+        <input type="hidden" name="calha" id="calha" value="0">
+        <input class="form-check-input" type="checkbox" name="calha" id="calha" value="1">
     </div>
 </td>`;
