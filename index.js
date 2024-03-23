6e37c92350e29d5fe5a64480809e005e64697ebd
LUIZ-FELIPE3393 <2023003393@ifam.edu.br>
2024-03-23 18:21:58 -0400

    Salvamento de checklist via cookies
    O site salva normalmente os dados do checklist via cookies

--- index.js
+++ index.js
@@ -1,67 +1,77 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
-const saveChecklist = require('./checklist/save_checklist');
+const saveChecklist = require("./checklist/save_checklist");
 
 let app = express();
 let server = http.createServer(app);
 
-server.listen(3030, function() {
-    console.log('O servidor já pode ser acessado em http://localhost:3030');
+server.listen(3030, function () {
+  console.log("O servidor já pode ser acessado em http://localhost:3030");
 });
 
 //Session
 
-
 //Bodyparser
-    app.use(express.urlencoded({extended: false}))
-    app.use(express.json());
-    app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
 
 //Bootstrap
-    app.use('/css', express.static('./bootstrap/css'));
-    app.use('/js', express.static('./bootstrap/js'));
-    app.use('/icon-font', express.static('./node_modules/bootstrap-icons/font'));
+app.use("/css", express.static("./bootstrap/css"));
+app.use("/js", express.static("./bootstrap/js"));
+app.use("/icon-font", express.static("./node_modules/bootstrap-icons/font"));
 
 //Express
-    app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, "./public")));
 
 //Rotas
-app.post('/save-checklist', function(req, res) {    
-    console.log(req.body.datadia);
-    res.cookie("cookieDatadia", req.body.datadia);
+app.post("/save-checklist", function (req, res) {
+  res.cookie("cookieDatadia", req.body.datadia);
+  res.cookie("cookieCaixa", req.body.caixa);
+  res.cookie("cookieGalao", req.body.galao);
+  res.cookie("cookieVaso", req.body.vaso);
+  res.cookie("cookieBalde", req.body.balde);
+  res.cookie("cookieGarrafa", req.body.garrafa);
+  res.cookie("cookiePneu", req.body.pneu);
+  res.cookie("cookiePiscina", req.body.piscina);
+  res.cookie("cookiePocas", req.body.pocas);
+  res.cookie("cookiePote", req.body.pote);
+  res.cookie("cookieEntulho", req.body.entulho);
+  res.cookie("cookieCalha", req.body.calha);
 
-    console.log(req.body.caixa);
-    res.cookie("cookieCaixa", req.body.caixa);
+  console.log(req.cookies);
 
-    console.log(req.body.galao);
-    res.cookie("cookieGalao", req.body.galao);
-
-    console.log(req.body.vaso);
-    res.cookie("cookieVaso", req.body.vaso);
-
-    console.log(req.body.balde);
-    res.cookie("cookieBalde", req.body.balde);
-    
-    console.log(req.body.pocas);
-    res.cookie("cookiePocas", req.body.pocas);
-
-    console.log(req.cookies);
-
-    res.redirect('/checklist');
+  res.redirect("/checklist");
 });
 
-app.get('/cookies',function(req, res){
-    res.json(req.cookies);
+app.get("/cookies", function (req, res) {
+  res.json(req.cookies);
 });
 
-app.get('/checklist', function(req,res,html) {
-    console.log('checklist');
-    res.sendFile(path.join(__dirname, './checklist/checklist.html'));
+app.get("/load", function (req, res) {
+    res.clearCookie("cookieDatadia");
+    res.clearCookie("cookieCaixa");
+    res.clearCookie("cookiePote");
+    res.clearCookie("cookieGarrafa");
+    res.clearCookie("cookieCalha");
+    res.clearCookie("cookiePocas");
+    res.clearCookie("cookieEntulho");
+    res.clearCookie("cookieGalao");
+    res.clearCookie("cookiePiscina");
+    res.clearCookie("cookiePneu");
+    res.clearCookie("cookieVaso");
+    res.clearCookie("cookieBalde");
+
+    res.redirect("/checklist");
 });
 
-app.get('/', function(req,res) {
-    res.sendFile(path.join(__dirname, './index.html'));
+app.get("/checklist", function (req, res, html) {
+  res.sendFile(path.join(__dirname, "./checklist/checklist.html"));
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
--- public/js/checklist.js
+++ public/js/checklist.js
@@ -1,32 +1,68 @@
-const addButton = document.querySelector('#add-checklist');
-const checklistTable = document.querySelector('#checklist-table');
+const addButton = document.querySelector("#add-checklist");
+const checklistTable = document.querySelector("#checklist-table");
 const form = document.querySelector("#formChecklist");
-const saveButtonWeb = document.getElementById('save-checklist-web');
+const saveButtonWeb = document.getElementById("save-checklist-web");
+const loadButton = document.getElementById("load-checklist");
+let cookieDataJson;
 
-let tupla_num = 1;
+let tupla_num = 0;
 
-const fetchCookies = fetch('/cookies').then( r => r.json() ).then( data => {
-    console.log('async');
+const fetchCookies = fetch("/cookies")
+  .then((r) => r.json())
+  .then((data) => {
     return data;
-});
+  });
 
-window.addEventListener('DOMContentLoaded', async function(e) {
+window.addEventListener("DOMContentLoaded", async function (e) {
     cookieDataJson = await fetchCookies;
-    console.log(cookieDataJson.cookieDatadia.length);
-    for(let i = 0; i < cookieDataJson.cookieDatadia.length - 1; i++) {
-        newTuple();
+    if(cookieDataJson.cookieDatadia == "undefined")
+        return;
+    for (let i = 0; i < cookieDataJson.cookieDatadia.length; i++) {
+        let tuple = document.createElement("tr");
+        tuple.setAttribute("id", "tupla-" + tupla_num);
+        tuple.setAttribute("name", "tupla");
+        tupla_num++;
+        tuple.innerHTML = tupleFormat;
+        tuple.querySelector("#datadia").value = cookieDataJson.cookieDatadia[i];
+
+        if (cookieDataJson.cookieCaixa[i] === "1")
+        tuple.querySelector("#caixa").checked = true;
+        if (cookieDataJson.cookieGalao[i] === "1")
+        tuple.querySelector("#galao").checked = true;
+        if (cookieDataJson.cookieVaso[i] === "1")
+        tuple.querySelector("#vaso").checked = true;
+        if (cookieDataJson.cookieBalde[i] === "1")
+        tuple.querySelector("#balde").checked = true;
+        if (cookieDataJson.cookiePocas[i] === "1")
+        tuple.querySelector("#pocas").checked = true;
+        if (cookieDataJson.cookieGarrafa[i] === "1")
+        tuple.querySelector("#garrafa").checked = true;
+        if (cookieDataJson.cookiePneu[i] === "1")
+        tuple.querySelector("#pneu").checked = true;
+        if (cookieDataJson.cookiePiscina[i] === "1")
+        tuple.querySelector("#piscina").checked = true;
+        if (cookieDataJson.cookiePote[i] === "1")
+        tuple.querySelector("#pote").checked = true;
+        if (cookieDataJson.cookieEntulho[i] === "1")
+        tuple.querySelector("#entulho").checked = true;
+        if (cookieDataJson.cookieCalha[i] === "1")
+        tuple.querySelector("#calha").checked = true;
+
+        checklistTable.append(tuple);
     }
 });
 
-form.addEventListener('submit', function(e) {
-    // Transforma valores de check nulo em 0
+form.addEventListener("submit", function (e) {
+  // Transforma valores de check nulo em 0
+
     for (let tuple of document.querySelectorAll("tr[name=tupla]")) {
         for (let checkbox of tuple.querySelectorAll("input[type=checkbox]")) {
+            console.log(tuple);
             if (!checkbox.checked) {
                 checkbox.value = 0;
             } else {
                 for (let hiddenNode of tuple.querySelectorAll("input[type=hidden]")) {
-                    if (hiddenNode.id == checkbox.id) {
+                    if (hiddenNode.name == checkbox.name) {
                         console.log(hiddenNode);
                         hiddenNode.disabled = true;
                     }
@@ -34,20 +70,19 @@ form.addEventListener('submit', function(e) {
             }
         }
     }
-
 });
 
-function newTuple() {
-    let tuple = document.createElement("tr");
-    tuple.setAttribute("id", "tupla-" + tupla_num);
-    tuple.setAttribute("name", "tupla");
-    tupla_num++;
-    tuple.innerHTML = tupleFormat; 
-    checklistTable.append(tuple);
-}
+loadButton.addEventListener("click", async function (e) {
+    location.replace("/load");
+});
 
-addButton.addEventListener('click', function(e) {
-    newTuple();
+addButton.addEventListener("click", function (e) {
+  let tuple = document.createElement("tr");
+  tuple.setAttribute("id", "tupla-" + tupla_num);
+  tuple.setAttribute("name", "tupla");
+  tupla_num++;
+  tuple.innerHTML = tupleFormat;
+  checklistTable.append(tuple);
 });
 
 const tupleFormat = `
@@ -58,67 +93,67 @@ const tupleFormat = `
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input type="hidden" name="caixa" id="caixa" value="0">
+        <input type="hidden" name="caixa" id="caixaHidden" value="0">
         <input class="form-check-input" type="checkbox" name="caixa" id="caixa" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input type="hidden" name="galao" id="galao" value="0">
+        <input type="hidden" name="galao" id="galaoHidden" value="0">
         <input class="form-check-input" type="checkbox" name="galao" id="galao" value="1">
     </div>
 </td>
 <td class="td-check">
-    <div class="form-check form-check-inline"> 
-        <input type="hidden" name="vaso" id="vaso" value="0">
+    <div class="form-check form-check-inline">
+        <input type="hidden" name="vaso" id="vasoHidden" value="0">
         <input class="form-check-input" type="checkbox" name="vaso" id="vaso" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input type="hidden" name="balde" id="balde" value="0">
+        <input type="hidden" name="balde" id="baldeHidden" value="0">
         <input class="form-check-input" type="checkbox" name="balde" id="balde" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input type="hidden" name="garrafa" id="garrafa" value="0">
+        <input type="hidden" name="garrafa" id="garrafaHidden" value="0">
         <input class="form-check-input" type="checkbox" name="garrafa" id="garrafa" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input type="hidden" name="pneu" id="pneu" value="0">
+        <input type="hidden" name="pneu" id="pneuHidden" value="0">
         <input class="form-check-input" type="checkbox" name="pneu" id="pneu" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input type="hidden" name="piscina" id="piscina" value="0">
+        <input type="hidden" name="piscina" id="piscinaHidden" value="0">
         <input class="form-check-input" type="checkbox" name="piscina" id="piscina" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input type="hidden" name="pocas" id="pocas" value="0">
+        <input type="hidden" name="pocas" id="pocasHidden" value="0">
         <input class="form-check-input" type="checkbox" name="pocas" id="pocas" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input type="hidden" name="pote" id="pote" value="0">
+        <input type="hidden" name="pote" id="poteHidden" value="0">
         <input class="form-check-input" type="checkbox" name="pote" id="pote" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input type="hidden" name="entulho" id="entulho" value="0">
+        <input type="hidden" name="entulho" id="entulhoHidden" value="0">
         <input class="form-check-input" type="checkbox" name="entulho" id="entulho" value="1">
     </div>
 </td>
 <td class="td-check">
     <div class="form-check form-check-inline">
-        <input type="hidden" name="calha" id="calha" value="0">
+        <input type="hidden" name="calha" id="calhaHidden" value="0">
         <input class="form-check-input" type="checkbox" name="calha" id="calha" value="1">
     </div>
 </td>`;
